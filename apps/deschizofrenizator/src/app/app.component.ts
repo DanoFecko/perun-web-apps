11494f4b99eabe5b0b4df003dae80ad060c1a34f
Daniel Fecko <dano9500@gmail.com>
2020-04-27 21:23:21 +0200

    dadadasdafsdf

--- apps/deschizofrenizator/src/app/app.component.ts
+++ apps/deschizofrenizator/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { StoreService } from '@perun-web-apps/perun/services';
+import { PerunPrincipal } from '@perun-web-apps/perun/openapi';
+// tslint:disable-next-line:nx-enforce-module-boundaries
+import { environment } from '../../../admin-gui/src/environments/environment';
 
 @Component({
   selector: 'perun-web-apps-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  public static minWidth = 992;
+
   title = 'deschizofrenizator';
+
+  constructor(private store: StoreService) {
+    this.getScreenSize(null);
+  }
+
+
+
+  lastScreenWidth: number;
+
+  principal: PerunPrincipal;
+  isProduction = false;
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.lastScreenWidth = window.innerWidth;
+  }
+
+  isMobile(): boolean {
+    return window.innerWidth <= AppComponent.minWidth;
+  }
+
+  ngOnInit(): void {
+    this.isProduction = environment.production;
+    this.principal = this.store.getPerunPrincipal();
+  }
+
 }
--- apps/deschizofrenizator/src/app/app.module.ts
+++ apps/deschizofrenizator/src/app/app.module.ts
@@ -1,5 +1,6 @@
+/* tslint:disable:nx-enforce-module-boundaries */
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, forwardRef, NgModule, Provider } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -8,9 +9,45 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { FirstGroupComponent } from './components/first-group/first-group.component';
 import { SecondGroupComponent } from './components/second-group/second-group.component';
 import { UiMaterialModule } from '@perun-web-apps/ui/material';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormField } from '@angular/material/form-field';
+import { AdminGuiConfigService } from '../../../admin-gui/src/app/core/services/common/admin-gui-config.service';
+import { ApiModule, Configuration, ConfigurationParameters } from '@perun-web-apps/perun/openapi';
+import { CustomIconService, StoreService } from '@perun-web-apps/perun/services';
+import { PERUN_API_SERVICE } from '@perun-web-apps/perun/tokens';
+import { ApiService } from '../../../admin-gui/src/app/core/services/api/api.service';
+import { ApiInterceptor } from '../../../admin-gui/src/app/core/services/api/ApiInterceptor';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SharedModule } from '../../../admin-gui/src/app/shared/shared.module';
+import { CoreModule } from '../../../admin-gui/src/app/core/core.module';
+import { MatIconModule } from '@angular/material/icon';
+import { GeneralModule } from '@perun-web-apps/general';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { AppRoutingModule } from './app-routing.module';
+
+export const API_INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useExisting: forwardRef(() => ApiInterceptor),
+  multi: true
+};
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function apiConfigFactory(store: StoreService): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: store.get('api_url')
+    // set configuration parameters here.
+  };
+  return new Configuration(params);
+}
+
+const loadConfigs = (appConfig: AdminGuiConfigService) => {
+  return () => {
+    return appConfig.loadConfigs();
+  };
+};
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, FirstGroupComponent, SecondGroupComponent],
@@ -21,8 +58,41 @@ import { MatFormField } from '@angular/material/form-field';
     UiMaterialModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    SharedModule,
+    CoreModule,
+    AppRoutingModule,
+    MatIconModule,
+    GeneralModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ApiModule
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [AdminGuiConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfigs,
+      multi: true,
+      deps: [AdminGuiConfigService]
+    },
+    {
+      provide: Configuration,
+      useFactory: apiConfigFactory,
+      deps: [StoreService]
+    },
+    CustomIconService,
+    {
+      provide: PERUN_API_SERVICE,
+      useClass: ApiService
+    },
+    ApiInterceptor,
+    API_INTERCEPTOR_PROVIDER],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
