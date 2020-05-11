4fb6d7cfeb36210aec3b27fda4047e9c879b8806
Daniel Fecko <dano9500@gmail.com>
2020-05-11 16:29:33 +0200

    dasdasda

--- apps/deschizofrenizator/src/app/app.component.ts
+++ apps/deschizofrenizator/src/app/app.component.ts
@@ -1,42 +1,40 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { StoreService } from '@perun-web-apps/perun/services';
 import { PerunPrincipal } from '@perun-web-apps/perun/openapi';
-// tslint:disable-next-line:nx-enforce-module-boundaries
-import { environment } from '../../../admin-gui/src/environments/environment';
 
 @Component({
   selector: 'perun-web-apps-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   public static minWidth = 992;
 
   title = 'deschizofrenizator';
 
-  constructor(private store: StoreService) {
-    this.getScreenSize(null);
-  }
-
-
-
-  lastScreenWidth: number;
-
-  principal: PerunPrincipal;
-  isProduction = false;
-
-  @HostListener('window:resize', ['$event'])
-  getScreenSize(event?) {
-    this.lastScreenWidth = window.innerWidth;
-  }
-
-  isMobile(): boolean {
-    return window.innerWidth <= AppComponent.minWidth;
-  }
-
-  ngOnInit(): void {
-    this.isProduction = environment.production;
-    this.principal = this.store.getPerunPrincipal();
-  }
+  // constructor(private store: StoreService) {
+  //   this.getScreenSize(null);
+  // }
+  //
+  //
+  //
+  // lastScreenWidth: number;
+  //
+  // principal: PerunPrincipal;
+  // isProduction = false;
+  //
+  // @HostListener('window:resize', ['$event'])
+  // getScreenSize(event?) {
+  //   this.lastScreenWidth = window.innerWidth;
+  // }
+  //
+  // isMobile(): boolean {
+  //   return window.innerWidth <= AppComponent.minWidth;
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.isProduction = environment.production;
+  //   this.principal = this.store.getPerunPrincipal();
+  // }
 
 }
--- apps/deschizofrenizator/src/environments/environment.prod.ts
+++ apps/deschizofrenizator/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   entitiesUrl: 'https://login1.cesnet.cz/proxy/module.php/perun/listOfMetadata.php',
-  api_url: 'http://localhost/krb/rpc/',
+  api_url: 'https://perun-dev.cesnet.cz/krb/rpc/',
 };
